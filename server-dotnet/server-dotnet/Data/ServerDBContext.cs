7b1339d9ab52198379bcde980f55ab15675f9a25
SAFRIZAL AJI <safrizalaji16@gmail.com>
2023-02-14 22:17:07 +0700

    init setting

---
+++ server-dotnet/server-dotnet/Data/ServerDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using server_dotnet.Models;
+
+namespace server_dotnet.Data
+{
+    public class ServerDBContext : DbContext
+    {
+        public ServerDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
